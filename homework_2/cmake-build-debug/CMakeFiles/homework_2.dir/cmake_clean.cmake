31f44f646258cefcb37dc3baf39f2e36c7af3ffd
Kaden Hellewell <kdex95@yahoo.com>
2021-10-04 13:26:37 -0600

    Starting repository

---
+++ homework_2/cmake-build-debug/CMakeFiles/homework_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_2.dir/main.cpp.obj"
+  "homework_2.exe"
+  "homework_2.exe.manifest"
+  "homework_2.pdb"
+  "libhomework_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework_2/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <random>
+
+// Arguments passed in (in this order)
+int thread_count;
+int bin_count;
+float min_meas;
+float max_meas;
+int data_count;
+int bin_size = ceil((double) (max_meas - min_meas) / (double) bin_count);
+
+void sort_num(float num)
+{
+
+}
+
+void threadFunction(float array[])
+{
+    for (int i = 0; i < data_count; i++)
+    {
+        sort_num(array[i]);
+    }
+}
+
+float get_random()
+{
+    float random = ((float) rand()) / (float) RAND_MAX; // return a float between 0 and 1
+
+    float range = max_meas - min_meas;
+    return (random*range) + min_meas;
+}
+
+// description here of what order things are passed in
+// for now, first arg is number of bin_size, second is data_size of array, third is number of threads
+int main(int argc, char* argv[])
+{
+    srand(100);
+    thread_count = (int) argv[0];
+    bin_count = (int) argv[1];
+    std::string min_meas_string(argv[2]);
+    min_meas = std::stof(min_meas_string);
+    std::string  max_meas_string(argv[3]);
+    max_meas = std::stof(max_meas_string);
+    data_count = (int) argv[4];
+
+    bin_size = (int) argv[0];
+    data_count = (int) argv[1];
+    float nums[data_count];
+    for(float k : nums)
+        k = get_random();
+
+    std::thread threads[thread_count];
+    for(int i = 0; i < thread_count; i++)
+    {
+        threads[i] = new std::thread(threadFunction, i);
+    }
+    for(int i = 0; i < thread_count; i++)
+    {
+        threads[i].join();
+    }
+
+
+    return 0;
+}
