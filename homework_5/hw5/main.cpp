0247624f7cc97348f540b6e54c10e35a68e0eb0a
Kaden Hellewell <kdex95@yahoo.com>
2021-11-29 18:08:23 -0700

    Got the program filled out; on to testing

--- homework_5/hw5/main.cpp
+++ homework_5/hw5/main.cpp
@@ -17,6 +17,7 @@ int data_count;
 float max_meas;
 float min_meas;
 float *bin_maxes;
+int local_n;
 
 // description here of what order things are passed in
 // 1. bin count
@@ -32,6 +33,7 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     auto* data = new float[data_count];
+    auto* filled_bins = new float[bin_count];
 
     //Get_arg includes broadcasting
     Get_arg(argc, argv);
@@ -40,15 +42,50 @@ int main(int argc, char* argv[]) {
     {
         srand(100);
         init_data(data_count, max_meas, min_meas, data);
-        MPI_Scatter(data, data_count, MPI_FLOAT, local_data, MPI_FLOAT, 0, comm);
+        MPI_Scatter(data, local_n, MPI_FLOAT, local_data, local_n, MPI_FLOAT, 0, comm);
+        for(int i = 0; i < bin_count; i++)
+        {
+            filled_bins[i] = 0;
+        }
         //TODO: receive data from other processes using MPI_Receive
+        for(int i = 0; i < comm_sz; i++)
+        {
+            float *incoming_bins = new float[local_n];
+            MPI_Recv(incoming_bins, local_n, MPI_FLOAT, i, MPI_ANY_TAG, comm);
+            for(int j = 0; j < bin_count; j++)
+                filled_bins[j] += incoming_bins[j];
+        }
+        std::cout << "The bins are: ";
+        for(int i = 0; i < bin_count; i++)
+        {
+            std::cout << filled_bins[i] << ", ";
+        }
+        std::cout << std::endl;
     }
     else
     {
-        MPI_Scatter(data, data_count, MPI_FLOAT, local_data, MPI_FLOAT, 0, comm);
+        MPI_Scatter(data, local_n, MPI_FLOAT, local_data, local_n, MPI_FLOAT, 0, comm);
+        float *local_bins = new float[bin_count];
+        //Initialize local bins to zero
+        for(int i = 0; i < local_n; i++)
+        {
+            local_bins[i] = 0;
+        }
         //TODO:
         //Do the sorting into bins here (local data into some local filled_bins).
         //Send the results back to process 0 using MPI_Send
+        for(int i = 0; i < local_n; i++)
+        {
+            for(int j = 0; j < bin_count; j++)
+            {
+                if (local_data[i] <= bin_maxes[j])
+                {
+                    local_bins[j]++;
+                    break;
+                }
+            }
+        }
+        MPI_Send(local_bins, local_n, MPI_FLOAT, 0, MPI_ANY_TAG, comm);
     }
 
     MPI_Finalize(&argc, &argv);
@@ -79,6 +116,7 @@ void Get_arg(int argc, char* argv[])
         std::string d_count_string(argv[4]);
         data_count = std::stoi(d_count_string);
 
+        local_n = data_count / comm_sz;
         init_bins(max_meas, min_meas, bin_count, bin_maxes);
     }
     MPI_Bcast(bin_count, 1, MPI_INT, 0, comm);
