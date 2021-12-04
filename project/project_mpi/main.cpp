d62f9cbe3c7ac82381b383ba715551227e97f972
Kaden Hellewell <kdex95@yahoo.com>
2021-12-03 21:06:45 -0700

    Got program to not crash. Still not quite working

--- project/project_mpi/main.cpp
+++ project/project_mpi/main.cpp
@@ -9,49 +9,29 @@
 
 using namespace std;
 
-struct Point_Vector{
+//These are separate structs to aid is differentiating points and vectors
+struct Vector{
     float x_val;
     float y_val;
 };
 
-vector<Point_Vector> vectors;
+struct Point{
+    float x_coord;
+    float y_coord;
+};
+
+vector<Vector> vectors;
 
 void get_args(int argc, char* argv[]);
-Point_Vector const_vect_mult(float c, Point_Vector v);
-Point_Vector get_v_from_field(int x_coord, int y_coord);
-Point_Vector get_v_from_field(float x_coord, float y_coord);
-Point_Vector get_v_from_field(Point_Vector p);
-Point_Vector add_vectors(Point_Vector v1, Point_Vector v2);
-
-/**
- * Given a position of a particle, find its next position.
- * @param x0 Current particle x position
- * @param y0 Current particle y position
- * @param x New x position
- * @param x_step time step
- * @return New y position
- */
-Point_Vector rungeKutta(Point_Vector pi, float x_step)
-{
-    Point_Vector k1{}, k2{}, k3{}, k4{};
-
-    // Apply Runge Kutta Formulas
-    // to find next value of y
-    k1 = const_vect_mult(x_step, get_v_from_field(pi));
-    Point_Vector v_1 = get_v_from_field(add_vectors(pi, const_vect_mult(.5, k1)));
-    k2 = const_vect_mult(x_step, v_1);
-    Point_Vector v_2 = get_v_from_field(add_vectors(pi, const_vect_mult(.5, k2)));
-    k3 = const_vect_mult(x_step, v_2);
-    Point_Vector v_3 = get_v_from_field(add_vectors(pi, k3));
-    k4 = const_vect_mult(x_step, v_3);
-    Point_Vector tempSum = k1;
-    tempSum = add_vectors(tempSum, const_vect_mult(2, k2));
-    tempSum = add_vectors(tempSum, const_vect_mult(2, k3));
-    tempSum = add_vectors(tempSum, k4);
-
-    return add_vectors(pi, const_vect_mult(0.1667, tempSum));
-}
-//Algorithm from: https://web.cs.ucdavis.edu/~ma/ECS177/papers/particle_tracing.pdf
+Vector const_vect_mult(float c, Vector v);
+Vector get_v_from_field(int x_coord, int y_coord);
+Vector get_v_from_field(float x_coord, float y_coord);
+Vector get_v_from_field(Point p);
+Vector add_vectors(Vector v1, Vector v2);
+Point add_vector_point(Point p, Vector v);
+Point rungeKutta(Point p, float x_step);
+bool not_in_range(Point p);
+Vector interpolate(Vector v1, Vector v2, int bigP, int smallP, float p);
 
 int comm_sz; //Number of Process
 int my_rank; //Rank of current process
@@ -74,35 +54,59 @@ int main(int argc, char* argv[]) {
 //    MPI_Comm_rank(comm, &my_rank);
 
     //Read in file
-    vector<float> buffer;
+    //vector<float> buffer;
     float f;
     std::ifstream inFile("cyl2d_1300x600_float32[2].raw", std::ios::binary);
-    //buffer contains all of the floats
-    while (inFile.read(reinterpret_cast<char*>(&f), sizeof(float)))
-        buffer.push_back(f);
 
-    //Set up vector of vectors
-    for(int i = 0; i < buffer.size(); i++)
-    {
-        Point_Vector thisVector{};
-        thisVector.x_val = buffer[i];
-        thisVector.y_val = buffer[++i];
-        vectors.push_back(thisVector);
+    float * buffer = nullptr;
+    if (inFile) {
+        // get length of file:
+        inFile.seekg (0, inFile.end);
+        int length = inFile.tellg();
+        inFile.seekg (0, inFile.beg);
+        buffer = new float[length / sizeof(float)];
+
+        std::cout << "Reading " << length << " characters... " << endl;
+        // read data as a block:
+        inFile.read ((char*)buffer, length);
+
+        if (inFile)
+            std::cout << "all characters read successfully." << endl;
+        else
+            std::cout << "error: only " << inFile.gcount() << " could be read";
+        inFile.close();
+
+
+        //Set up vector of vectors
+        for(int i = 0; i < length / sizeof(float); i++)
+        {
+            Vector thisVector{};
+            thisVector.x_val = buffer[i];
+            thisVector.y_val = buffer[++i];
+            vectors.push_back(thisVector);
+        }
     }
 
-    int initial_y = 300;
-    float time_step = .1;
-    int initial_x = 0;
+
+    float initial_y = 0;
+    float time_step = .5;
+    float initial_x = 0;
     cout << "line_id, coordinate_x, coordinate_y" << endl;
+    Point current{};
+    current.x_coord = initial_x;
+    current.y_coord = initial_y;
     for(int row = 0; row < 10; row++)
     {
         int lineId = row;
-        cout << lineId << ", ";
-        for(int col = 0; col < 10; col++)
+        for(int col = 0; col < 10; col++)//this loop doesn't need to depend on columns, some lines loop in vortex. pick stopping time
         {
-            Point_Vector current = get_v_from_field(col, row);
-//            current = rungeKutta(current, time_step);
-            cout << current.x_val << ", " << current.y_val << endl;
+            current = rungeKutta(current, time_step);
+            if(not_in_range(current))
+            {
+                //The streamline has left the known vector field. Go to the next line.
+                break;
+            }
+            cout << lineId << ", " << current.x_coord << ", " << current.y_coord << endl;
         }
     }
 
@@ -111,64 +115,138 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-Point_Vector get_v_from_field(int x_coord, int y_coord)
+Vector get_v_from_field(int x_coord, int y_coord)
 {
     int index = y_coord*data_cols + x_coord;
     return vectors[index];
 }
 
-Point_Vector get_v_from_field(float x_coord, float y_coord)
+Vector get_v_from_field(float x_coord, float y_coord)
 {
-    //Get element index from coordinates
+    //Bilinear Interpolation
+    //Get integer points around the given x and y
     int floor_y = floor(y_coord);
     int floor_x = floor(x_coord);
     int ceil_y = ceil(y_coord);
     int ceil_x = ceil(x_coord);
 
-    //Q11 - bottom left; Q12 - top left; Q21 - bottom right; Q22 - top right
-    //These are velocity vectors, not points
-    Point_Vector Q11 = get_v_from_field(floor_x, floor_y);
-    Point_Vector Q12 = get_v_from_field(floor_x, ceil_y);
-    Point_Vector Q21 = get_v_from_field(ceil_x, floor_y);
-    Point_Vector Q22 = get_v_from_field(ceil_x, ceil_y);
+    if(ceil_x == floor_x && ceil_y == floor_y)//both are integers, no interpolation
+    {
+        return get_v_from_field((int)x_coord, (int)y_coord);
+    }
 
+    Vector R1{}, R2{};
+    //Linear interpolation
+    if(ceil_x == floor_x) //x is an integer, y is not
+    {
+        R1 = get_v_from_field((int)x_coord, floor_y);
+        R2 = get_v_from_field((int)x_coord, ceil_y);
+        return interpolate(R1, R2, ceil_y, floor_y, y_coord);
+    }
+
+    if(ceil_y == floor_y)//y is an integer, x is not
+    {
+        R1 = get_v_from_field(floor_x, (int)y_coord);
+        R2 = get_v_from_field(ceil_x, (int)y_coord);
+        return interpolate(R1, R2, ceil_x, floor_x, x_coord);
+    }
+
+    //Neither are integers
     //bilinear interpolation
-    //Calculate R1
-    Point_Vector temp1 = const_vect_mult((ceil_x - x_coord)/(ceil_x - floor_x), Q11);
-    Point_Vector temp2 = const_vect_mult((x_coord - floor_x)/(ceil_x - floor_x), Q21);
-    Point_Vector R1 = add_vectors(temp1, temp2);
+    //Q11 - bottom left; Q12 - top left; Q21 - bottom right; Q22 - top right
+    Vector Q11 = get_v_from_field(floor_x, floor_y);
+    Vector Q12 = get_v_from_field(floor_x, ceil_y);
+    Vector Q21 = get_v_from_field(ceil_x, floor_y);
+    Vector Q22 = get_v_from_field(ceil_x, ceil_y);
+
+    //Calculate R10
+    R1 = interpolate(Q11, Q21, ceil_x, floor_x, x_coord);
 
     //Calculate R2
-    temp1 = const_vect_mult((ceil_x - x_coord)/(ceil_x - floor_x), Q12);
-    temp2 = const_vect_mult((x_coord - floor_x)/(ceil_x - floor_x), Q22);
-    Point_Vector R2 = add_vectors(temp1, temp2);
+    R2 = interpolate(Q12, Q22, ceil_x, floor_x, x_coord);
 
     //Calculate P
-    temp1 = const_vect_mult((ceil_y - y_coord)/(ceil_y - floor_y), R1);
-    temp2 = const_vect_mult((y_coord - floor_y)/(ceil_y - floor_y), Q21);
-    Point_Vector P = add_vectors(temp1, temp2);
-    return P;
+    return interpolate(R1, R2, ceil_y, floor_y, y_coord);
+}
+/*
+ * How to calculate R1, R2, and P (from https://x-engineer.org/bilinear-interpolation/)
+ * \[R_{1}(x, y) = Q_{11} \frac{x_{2}-x}{x_{2}-x_{1}} + Q_{21} \frac{x-x_{1}}{x_{2}-x_{1}} \tag{1}\]
+
+   \[R_{2}(x, y) = Q_{12} \frac{x_{2}-x}{x_{2}-x_{1}} + Q_{22} \frac{x-x_{1}}{x_{2}-x_{1}} \tag{2}\]
+
+   \[{P(x,y) = R_{1} \frac{y_{2}-y}{y_{2}-y_{1}} + R_{2} \frac{y-y_{1}}{y_{2}-y_{1}}} \tag{3}\]
+ */
+
+Vector interpolate(Vector v1, Vector v2, int bigP, int smallP, float p)
+{
+    Vector temp1 = const_vect_mult((bigP - p) / (bigP - smallP), v1);
+    Vector temp2 = const_vect_mult((p - smallP) / (bigP - smallP), v2);
+    Vector returnVector = add_vectors(temp1, temp2);
+    return returnVector;
 }
 
-Point_Vector get_v_from_field(Point_Vector p)
+Vector get_v_from_field(Point p)
 {
-    return get_v_from_field(p.x_val, p.y_val);
+    return get_v_from_field(p.x_coord, p.y_coord);
 }
 
-Point_Vector const_vect_mult(float c, Point_Vector v)
+Vector const_vect_mult(float c, Vector v)
 {
-    Point_Vector returnVector{};
+    Vector returnVector{};
     returnVector.x_val = c*v.x_val;
     returnVector.y_val = c*v.y_val;
     return returnVector;
 }
 
-Point_Vector add_vectors(Point_Vector v1, Point_Vector v2)
+Vector add_vectors(Vector v1, Vector v2)
 {
-    Point_Vector returnVector{};
+    Vector returnVector{};
     returnVector.x_val = v1.x_val + v2.x_val;
     returnVector.y_val = v1.y_val + v2.y_val;
     return returnVector;
+}
+
+Point add_vector_point(Point p, Vector v)
+{
+    Point returnPoint{};
+    returnPoint.x_coord = p.x_coord + v.x_val;
+    returnPoint.y_coord - p.y_coord + v.y_val;
+    return returnPoint;
+}
+
+Point rungeKutta(Point p, float x_step)
+{
+    Vector k1{}, k2{}, k3{}, k4{};
+
+    // Apply Runge Kutta Formulas
+    // to find next value of y
+    k1 = const_vect_mult(x_step, get_v_from_field(p));
+    Point p1 = add_vector_point(p, const_vect_mult(.5, k1));
+    Vector v_1 = get_v_from_field(p1);
+
+    k2 = const_vect_mult(x_step, v_1);
+    Point p2 = add_vector_point(p, const_vect_mult(.5, k2));
+    Vector v_2 = get_v_from_field(p2);
+
+    k3 = const_vect_mult(x_step, v_2);
+    Point p3 = add_vector_point(p, k3);
+    Vector v_3 = get_v_from_field(p3);
+
+    k4 = const_vect_mult(x_step, v_3);
+    Vector tempSum = k1;
+    tempSum = add_vectors(tempSum, const_vect_mult(2, k2));
+    tempSum = add_vectors(tempSum, const_vect_mult(2, k3));
+    tempSum = add_vectors(tempSum, k4);
+
+    return add_vector_point(p, const_vect_mult(0.1667, tempSum));
+}
+//Algorithm from: https://web.cs.ucdavis.edu/~ma/ECS177/papers/particle_tracing.pdf
+
+bool not_in_range(Point p)
+{
+    if(p.x_coord < 0 || p.x_coord >= data_cols || p.y_coord < 0 || p.y_coord >= data_rows)
+        return true;
+    return false;
 }
 
 /**
